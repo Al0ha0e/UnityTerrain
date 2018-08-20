066744079d6f6d8b71e9e68889dd2fce90b5c177
Al0ha0e <1919270783@qq.com>
2018-08-20 22:02:58 +0800

    Clear

--- Assets/test1.cs
+++ Assets/test1.cs
@@ -12,12 +12,12 @@ public class test1 : MonoBehaviour
         cb = new ComputeBuffer(4, 12);
         Kernel = cs.FindKernel("CSMain");
         cs.SetBuffer(Kernel, "cb", cb);
-        Vector3 sbb = new Vector3(2.0f, 5.0f, 4.0f);
-        cs.SetVector("SB", sbb);
+        Vector3 test = new Vector3(2.0f, 5.0f, 4.0f);
+        cs.SetVector("TEST", test);
         cs.Dispatch(Kernel, 1, 1, 4);
         Vector3[] ans = new Vector3[4];
         cb.GetData(ans);
-        foreach (Vector3 sb in ans) print(sb);
+        foreach (Vector3 a in ans) print(a);
 	}
         void Update()
         {
